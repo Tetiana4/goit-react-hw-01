ae4c81627748a9aad2750355e1bc35e0924b0ff9
Tetiana Shevchuk <shevchuktetiana4@gmail.com>
2021-08-26 08:58:33 +0200

    Добавляет рабочие файлы

---
+++ src/App.js
@@ -0,0 +1,51 @@
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+import Profile from './components/profile'
+import user from './user.json';
+import statisticalData from './statistical-data.json';
+import Statistics from './statistics/statistics';
+
+
+export default function App() {
+  return (
+    <>
+    <Profile
+      name={user.name}
+      tag={user.tag}
+      location={user.location}
+      avatar={user.avatar}
+      stats={user.stats}
+    />
+
+      <Statistics
+        
+        title="Upload stats"
+        stats={statisticalData} />
+{/* <Statistics stats={statisticalData.stats} /> */}
+      </>
+  );
+}
+
